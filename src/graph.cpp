92ca39a4fa234dc626f6e9d920934f0eb8e9673c
dominc8 <kormandominik@gmail.com>
2021-10-22 13:23:42 +0200

    refactor graph to allocate additional nodes

--- src/graph.cpp
+++ src/graph.cpp
@@ -2,11 +2,19 @@
 #include "logger.hpp"
 #include <stdlib.h>
 
-bool graph::init(int32_t n_node)
+bool graph::init(int32_t n_node, int32_t n_component_node)
 {
-    matrix = static_cast<uint8_t *>(calloc(n_node * n_node, sizeof(*matrix)));
+    /* allocate additional n_component_node nodes*/
+    int32_t all_nodes = n_node + n_component_node;
+    matrix = static_cast<uint8_t *>(calloc(all_nodes * all_nodes, sizeof(*matrix)));
     this->n_node = (nullptr != matrix) ? n_node : 0;
+    this->n_all_node = (nullptr != matrix) ? n_all_node : 0;
     return nullptr != matrix;
+}
+
+void graph::deinit(void)
+{
+    free(matrix);
 }
 
 uint8_t& graph::edge_of(int32_t p0, int32_t p1)
@@ -18,3 +26,4 @@ uint8_t graph::edge_of(int32_t p0, int32_t p1) const
 {
     return matrix[p0 * n_node + p1];
 }
+
