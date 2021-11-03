9c12fbb5abb15bb20d1a2b1c26ec99e3164b3aea
dominc8 <kormandominik@gmail.com>
2021-11-03 15:34:32 +0100

    More ogdf separation

--- inc/ogdf_utils.hpp
+++ inc/ogdf_utils.hpp
@@ -3,17 +3,15 @@
 
 #include "graph.hpp"
 #include "node.hpp"
-//namespace ogdf
-//{
-//    class Graph;
-//    class node;
-//    //template <class arg> class Array;
-//    template <class E, class INDEX = int> class Array;
-//}
-#include <ogdf/basic/Array.h>
-#include <ogdf/basic/Graph.h>
 
-void create_edges(ogdf::Graph &G, ogdf::Array<ogdf::node> &v, const graph &g);
+namespace ogdf
+{
+    class Graph;
+    using node = class NodeElement*;
+    template <class E, class INDEX> class Array;
+}
+
+void create_edges(ogdf::Graph &G, ogdf::Array<ogdf::node, int> &v, const graph &g);
 void save_layout_svg(const graph &g, const std::vector<node> &nodes, const char *filename);
 void ogdf_layout(graph &g, std::vector<node> &nodes);
 
