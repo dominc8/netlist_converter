9b87957c44a210e3470706f429fb77eab4f2b9dd
dominc8 <kormandominik@gmail.com>
2021-11-04 18:15:28 +0100

    add get_neighbours ut

--- ut/layoutizer.cpp
+++ ut/layoutizer.cpp
@@ -10,6 +10,14 @@ protected:
         bool is_in_corner = node_is_in_corner(n, n1, n2);
         EXPECT_EQ(is_in_corner, expected_flag);
     }
+    void check_neighbours(const graph &g, int32_t node_idx, int32_t neighbour1, int32_t neighbour2)
+    {
+        int32_t neighbours[2];
+        get_neighbours(g, node_idx, &neighbours[0]);
+        bool are_proper_neighbours = ((neighbour1 == neighbours[0]) && (neighbour2 == neighbours[1])) ||
+                                     ((neighbour1 == neighbours[1]) && (neighbour2 == neighbours[0]));
+        EXPECT_EQ(are_proper_neighbours, true);
+    }
 };
 
 
@@ -81,5 +89,29 @@ TEST_F(layoutizer_tests, node_is_in_corner)
     n3.set_coord(20, 10);
     check_node_in_corner(n2, n1, n3, true);
     check_node_in_corner(n2, n3, n1, true);
+}
+
+TEST_F(layoutizer_tests, get_neighbours)
+{
+    /*
+     * 0 - 1
+     * |   |
+     * 2 - 3
+     * */
+    graph g;
+    g.init(4, 2);
+    g.edge_of(0, 1) = 1;
+    g.edge_of(0, 2) = 1;
+    g.edge_of(1, 0) = 1;
+    g.edge_of(2, 0) = 1;
+    g.edge_of(3, 1) = 1;
+    g.edge_of(3, 2) = 1;
+    g.edge_of(1, 3) = 1;
+    g.edge_of(2, 3) = 1;
+
+    check_neighbours(g, 0, 1, 2);
+    check_neighbours(g, 1, 0, 3);
+    check_neighbours(g, 2, 0, 3);
+    check_neighbours(g, 3, 1, 2);
 }
 
